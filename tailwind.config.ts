b5b28a4438b4eb2895a096453e881b43de42d3bf
Nadeem Shareef <nadeemshareef934@gmail.com>
2024-06-24 18:50:13 +0530

    add tailwind forms plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -82,7 +82,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/forms")],
 } satisfies Config;
 
 export default config;
