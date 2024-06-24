90c01b12c0be964ea0dff07d80a4aad307d56f79
Nadeem Shareef <nadeemshareef934@gmail.com>
2024-06-24 23:29:30 +0530

    fix: timeout error

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -4,52 +4,60 @@ import { z } from "zod";
 import bcrypt from "bcrypt";
 
 export async function POST(request: NextRequest) {
-  const schema = z.object({
-    email: z.string().email(),
-    password: z.string().min(1, "required"),
-  });
-  const payload = await request.json();
-  const parseResult = schema.safeParse(payload);
+  try {
+    const schema = z.object({
+      email: z.string().email(),
+      password: z.string().min(1, "required"),
+    });
+    const payload = await request.json();
+    const parseResult = schema.safeParse(payload);
 
-  if (!parseResult.success) {
-    return new Response(
-      parseResult.error.errors.map((e) => `${e.path} ${e.message}`).join(" , "),
-      { status: 400 },
+    if (!parseResult.success) {
+      return new Response(
+        parseResult.error.errors
+          .map((e) => `${e.path} ${e.message}`)
+          .join(" , "),
+        { status: 400 },
+      );
+    }
+
+    const client = await clientPromise;
+
+    if (!client) {
+      return new Response("Error connecting to MongoDB", { status: 500 });
+    }
+
+    const db = client.db("db");
+
+    const user = await db
+      .collection("users")
+      .findOne({ email: parseResult.data.email });
+
+    if (!user) {
+      return new Response(JSON.stringify({ message: "User not found" }), {
+        status: 400,
+      });
+    }
+
+    const isValidPassword = await bcrypt.compare(
+      parseResult.data.password,
+      user.password,
     );
-  }
 
-  const client = await clientPromise;
+    if (!isValidPassword) {
+      return new Response(JSON.stringify({ message: "Invalid password" }), {
+        status: 400,
+      });
+    }
 
-  if (!client) {
-    return new Response("Error connecting to MongoDB", { status: 500 });
-  }
-
-  const db = client.db("db");
-
-  const user = await db
-    .collection("users")
-    .findOne({ email: parseResult.data.email });
-
-  if (!user) {
-    return new Response(JSON.stringify({ message: "User not found" }), {
+    return new Response(JSON.stringify({ message: "Login successful", user }), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ message: "Error logging in" }), {
       status: 400,
     });
   }
-
-  const isValidPassword = await bcrypt.compare(
-    parseResult.data.password,
-    user.password,
-  );
-
-  if (!isValidPassword) {
-    return new Response(JSON.stringify({ message: "Invalid password" }), {
-      status: 400,
-    });
-  }
-
-  return new Response(JSON.stringify({ message: "Login successful", user }), {
-    status: 200,
-  });
 }
 
 export async function GET() {
@@ -69,7 +77,6 @@ export async function GET() {
 
     return new Response(JSON.stringify(users), { status: 200 });
   } catch (error) {
-    console.error(error);
     return new Response(JSON.stringify({ message: "Error fetching users" }), {
       status: 500,
     });
